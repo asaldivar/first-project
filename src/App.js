cbc928b3f00498579acb6245f70c1167daf604db
Typhaine Myers <typhainemyers@typhaines-mbp.lan>
2020-08-21 13:39:45 -0700

    Change text in App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>make a change</p>
+        <p>Typhaine was here</p>
         <a
           className="App-link"
           href="https://reactjs.org"
